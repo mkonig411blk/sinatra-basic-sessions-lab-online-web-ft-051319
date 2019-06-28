c4055028b91bbf078352610e712f32357be779d4
Margot Konig <margot.konig@blackrock.com>
2019-06-28 02:44:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-configure do
-  enable :sessions
-  set :session_secret, "secret"
-end
+  
+  get '/hey' do 
+    @session = session
+  end
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
 end
